3bea1f2797427522c7c004d1ea8882650f56f71e
u-tain <korchevnuktc@gmail.com>
2023-09-12 22:08:28 +0300

    create upload data func

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 FROM python:3.10
 
 WORKDIR /app
-
+ENV DB_HOST 172.17.0.2
+ENV DB_USER default
 COPY requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
 COPY . .
+RUN ["python", "src/bd_utils.py"]
 RUN ["python", "src/preprocess.py"]
 RUN ["python", "src/train.py"]
 RUN ["python", "src/predict.py"]
--- src/bd_utils.py
+++ src/bd_utils.py
@@ -1,5 +1,30 @@
 import clickhouse_connect
-import os
+import os,sys
+import pandas as pd
+sys.path.insert(1, os.path.join(os.getcwd(), "data"))
 
 def connect2bd():
     return clickhouse_connect.get_client(host=os.getenv("DB_HOST"), username=os.getenv("DB_USER"), password='',)
+
+def upload_data():
+    client = connect2bd()
+    # загружаем в базу данных данные для обучения
+    querry1 = 'CREATE TABLE IF NOT EXISTS BBC_News_Train ( `Articled` Int, `Text` String, `Category` String) ENGINE = MergeTree ORDER BY Articled'
+    client.query(querry1)
+    df = pd.read_csv(os.path.join(os.getcwd(),'data','BBC News Train.csv'))
+    rows = list(df.itertuples(index=False, name=None))
+    insert_querry1 = 'INSERT INTO BBC_News_Train VALUES '+str(rows).replace('[','').replace(']','')
+    client.query(insert_querry1)
+
+    # загружаем в базу данных данные для теста
+    
+    querry2 = 'CREATE TABLE IF NOT EXISTS BBC_News_Test ( `Articled` Int, `Text` String) ENGINE = MergeTree ORDER BY Articled'
+    client.query(querry2)
+    df = pd.read_csv(os.path.join(os.getcwd(),'data','BBC News Test.csv'))
+    rows = list(df.itertuples(index=False, name=None))
+    insert_querry2 = 'INSERT INTO BBC_News_Test VALUES '+str(rows).replace('[','').replace(']','')
+    client.query(insert_querry2)
+
+
+if __name__ == "__main__":
+    upload_data()
