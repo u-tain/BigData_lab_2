b19bed12020eb4b3b8a36f90701137d332d2a942
Татьяна <43996253+u-tain@users.noreply.github.com>
2023-09-14 01:02:33 +0300

    Update bd_utils.py

--- src/bd_utils.py
+++ src/bd_utils.py
@@ -6,7 +6,6 @@ HOST = 'localhost' # os.getenv("DB_HOST")
 USER = 'default' #os.getenv("DB_USER")
 print(HOST, USER)
 print(os.getenv("DB_HOST"), os.getenv("DB_USER"))
-print(os.environ("DB_HOST"))
 def connect2bd():
     return clickhouse_connect.get_client(host=HOST, username=os.getenv("DB_USER"), password='', )
 
