231edb166fb43268db916f6ce2c74e91d5df7739
u-tain <korchevnuktc@gmail.com>
2023-09-30 23:04:51 +0300

    new image

--- src/bd_utils.py
+++ src/bd_utils.py
@@ -8,7 +8,7 @@ print(socket.gethostbyname(socket.gethostname()))
 
 
 def connect2bd():
-    return clickhouse_connect.get_client(host='localhost',
+    return clickhouse_connect.get_client(host=os.getenv("DB_HOST"),
                                          username=os.getenv("DB_USER"), 
                                          password=os.getenv("DB_PASS"))
 
